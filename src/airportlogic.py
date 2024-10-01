7c11737cab7f3a465843f556e70e7e310f12abfc
AtLobster <arafsambi@gmail.com>
2024-10-01 09:25:07 +0300

    airportlogic.py reworkings
    nyt toimii toi etäisyys eikä pitäisi varmaan tulla samoja maita

--- src/airportlogic.py
+++ src/airportlogic.py
@@ -1,3 +1,12 @@
+### TODO
+### mahdollisesti koita grind getGuesses hakemaan n määrä maita
+###
+### kommentit
+### yksi lista josta tehdä toi arvaus
+### eli randAp + selectedCountries
+
+
+
 import random
 import databases
 from geopy.distance import great_circle
@@ -11,77 +20,116 @@ def randomAirport():
     WHERE airport.type LIKE 'large_airport%' 
     ORDER BY rand() 
     LIMIT 1"""
-    #Valitaan satunnainen lentokenttä ja maa, jossa lentokenttä sijaitsee
+    # Haetaan lentokenttä, maa(jossa ap on) ja koordinaatit.
 
     cursor = databases.conn.cursor()
     cursor.execute(sql)
-
-    #Halutaan ulos satunnaisen lentokentän nimi ja maa
     randAirport = cursor.fetchall()
-    airportName = randAirport[0][0]
-    countryName = randAirport[0][1]
-    startPoint = (randAirport[0][2], randAirport[0][3])
+    rand_airportName = randAirport[0][0]
+    rand_countryName = randAirport[0][1]
+    rand_startPoint = (randAirport[0][2], randAirport[0][3])
+    # Tehdään sql-haulla saaduista tiedoista lista randAirport ja siirretään tiedot käyttöä varten jokaisesta sarakkeesta omaan muuttujaan.
+    # Sanakirja voisi toimia yleisemmin, mutta toisessa funktiossa se olisi turhan monimutkaista.
 
-    #print(airportName, countryName)
-    return airportName, countryName, startPoint
+    return rand_airportName, rand_countryName, rand_startPoint
 
-def wrongCountry(exceptionCountry):
+def distance(startPoint, destPoint):
+    #Tällä lasketaan etäisyys lähtömaan ja muiden maiden välillä.
+    dist = great_circle(startPoint, destPoint).kilometers
+    return dist
+
+def getGuesses(exceptionCountry, startPoint):
 
     sql = """
-    SELECT DISTINCT country.name, latitude_deg, longitude_deg
+    SELECT country.name, latitude_deg, longitude_deg
     from country
     JOIN airport ON country.iso_country = airport.iso_country
     WHERE airport.type LIKE 'large_airport%' AND country.name != '{exceptionCountry}'
+    GROUP BY country.name
     ORDER BY rand()
-    LIMIT 5
+    LIMIT 30
     """
+    # Haetaan 30 maata, joissa "large_airport" ja lentokenttien koordinaatit
 
     cursor = databases.conn.cursor()
     cursor.execute(sql)
+    countries = cursor.fetchall()
+    # Tehdään lista sql-haulla saaduista tiedoista.
 
-    wrongCountries = cursor.fetchall()
+    # Lista, jossa maat, koordinaatit ja etäisyys alkupisteeseen.
+    distances = []
+    for country in countries:
+        countryName = country[0]
+        coords = (country[1], country[2])
+        dist = distance(startPoint, coords)
+        distances.append((countryName, coords, dist))
+    #List comprehension??
 
-    return [(country[0], country[1], country[2]) for country in wrongCountries]
+    #print(distances)
 
-def distance(startPoint, destPoint):
+    # Järjestetään etäisyyksien mukaan ja valitaan lähimmästä viidestä 2
+    closestCountries = sorted(distances, key=lambda x: x[2])[:5]
+    selectedClosest = random.sample(closestCountries, 2)
 
-    alku = (startPoint)
-    loppu = (destPoint)
-    dist = great_circle(alku, loppu).kilometers
+    # Lista jäljellä olevista maista poissulkien lähimmät valitut.
+    remainingCountries = []
+    for country in distances:
+        if country not in selectedClosest:
+            remainingCountries.append(country)
+    # List comprehension when??
 
-    return dist
+    #
+    # ^^ vois vaa valita distances listasta ja laittaa exclude
+    # maat jotka on selectedClosest ??
+    #
+    #
 
-airportName, countryName, startPoint = randomAirport()
-wNamesAndCoords = wrongCountry(countryName)
-wNames = [country[0] for country in wNamesAndCoords]
 
-countryChoices = [countryName] + wNames
-random.shuffle(countryChoices)
+    selectedRemaining = random.sample(remainingCountries, 3)
+    finalSelection = selectedClosest + selectedRemaining # Lopullinen lista maista
 
-startLat, startLon = startPoint
-print(f'Starting point: {countryName}, {airportName}, ({startLat}, {startLon})\n')
+    # Pitäiskö tää saada palauttaa se määrä maita jonka haluan?
 
-for country in wNamesAndCoords:
+    return finalSelection, distances
+
+
+rand_airportName, rand_countryName, rand_startPoint = randomAirport()
+selectedCountries, allCountries = getGuesses(rand_airportName, rand_startPoint)
+
+
+selectedCountryNames = []
+for country in selectedCountries:
+    selectedCountryNames.append(country[0])
+# list comprehension bruda
+
+guesses = [rand_countryName] + selectedCountryNames
+random.shuffle(guesses)
+print(guesses)
+
+
+
+# Alla koodin testailua varten
+#
+#
+#
+#
+#
+
+startLat, startLon = rand_startPoint
+#print(f'Starting point: {rand_countryName}, {rand_airportName}, ({startLat}, {startLon})\n')
+
+
+
+
+for country in selectedCountries:
     destName = country[0]
-    destPoint = (country[1], country[2])
-    distancefrom = distance(startPoint, destPoint)
-    print(f'Maassa {destName} sijaitsevan lentokentän etäisyys lähtöpisteestä on {distancefrom: .0f}km. Päätepisteen koordinaatit ovat {destPoint}. ')
-
-print(f"\n{countryChoices}")
+    destPoint = country[1]
+    distanceFrom = country[2]
+    #print(f'Maassa {destName} sijaitsevan lentokentän etäisyys lähtöpisteestä on {distanceFrom: .0f}km. Päätepisteen koordinaatit ovat {destPoint}. ')
 
 
 
-"""
-
-Teeppä kommentit.
-tehdä tapa että pelaaja voi valita noista vaihtoehdoista
-muita juttuja en jake pohtia
-
-seuraavaks pitää tehä logiikka et toi valitsee vääristä maista x määrän y etäisyyden päästä. 
 
 
-jossai vaiheessa muuta randairport ja wrongcountries 1 funktioksi 
-sen pitäisi valita yksi niistä olemaan aloitus paikka.
-hmm
-"""
+
 
