8fd9948ce81f53b9eea3940fb2579104d7151283
Juho Molin <juho.molin@metropolia.fi>
2024-10-06 21:53:19 +0300

    Added functionality to run the entire project through the main.py file.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,21 @@
 import databases as db
 import accountlogic
 import mainmenu
-from src.accountlogic import create_account
-from src.mainmenu import print_logo, menu_selection, command_to_main_menu_check, command_to_difficulty
+from src.accountlogic import create_account, login
+from src.mainmenu import run_main_menu
 
 
 def main() -> None:
+    while True:
+        select = input("Syötä 1 jos haluat luoda käyttäjän, 2 jos haluat kirjautua sisään. ")
+        if select == "1":
+            create_account()
+        elif select == "2":
+            login()
+        else:
+            print("Virheellinen syöte, yritä uudestaan.")
 
-    print("1")
-
-    select = input("Syötä 1 jos haluat luoda käyttäjän, 2 jos haluat kirjautua sisään. ")
-
-    if select == "1":
-        create_account()
-    elif select == "2":
-        print("Tule sisään : ")
-        #login()
-    else:
-        print("Väärin meni :(")
-
-    print_logo()
-    menu_selection()
-    command_to_main_menu_check()
-    command_to_difficulty()
+    run_main_menu()
 
 
 if __name__ == '__main__':
