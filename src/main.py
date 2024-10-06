ba9636d285a971b86eb79e9f3242bccd43ce3abb
Atte Sten <arafsambi@gmail.com>
2024-10-06 21:34:42 +0300

    :)

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
 import databases as db
+import accountlogic
+import mainmenu
+from src.accountlogic import create_account
+from src.mainmenu import print_logo, menu_selection, command_to_main_menu_check, command_to_difficulty
+
 
 def main() -> None:
 
-    print("Hello, World!\n")
+    print("1")
+
+    select = input("Syötä 1 jos haluat luoda käyttäjän, 2 jos haluat kirjautua sisään. ")
+
+    if select == "1":
+        create_account()
+    elif select == "2":
+        print("Tule sisään : ")
+        #login()
+    else:
+        print("Väärin meni :(")
+
+    print_logo()
+    menu_selection()
+    command_to_main_menu_check()
+    command_to_difficulty()
+
 
 if __name__ == '__main__':
     main()
