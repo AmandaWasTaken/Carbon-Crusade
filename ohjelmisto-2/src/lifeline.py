21ed5a7eb56c27ceedd93d1193a12f86538daac7
jerep <jerepun@metropolia.fi>
2024-12-11 15:50:57 +0200

    lifeline modified

--- ohjelmisto-2/src/lifeline.py
+++ ohjelmisto-2/src/lifeline.py
@@ -1,11 +1,11 @@
 import random
 
-import airportlogic
-import gameplay
+# import airportlogic
+# import gameplay
 
 def fiftyfifty(country_options, correct_answer):
     incorrect_countries = []
-    remaining_incorrect_country = random.randint(0, len(country_options) - 2)
+    remaining_incorrect_country = []
     remaining_countries = []
 
     for country in country_options:
@@ -13,12 +13,16 @@ def fiftyfifty(country_options, correct_answer):
             incorrect_countries.append(country)
         if country == correct_answer:
             remaining_countries.append(country)
-    for i in range(0,len(incorrect_countries)):
-        if i == remaining_incorrect_country:
-            remaining_countries.append(incorrect_countries[i])
+
+    while len(remaining_countries) != int(len(countries) / 2):
+        random_incorrect_country = random.choice(incorrect_countries)
+        if random_incorrect_country not in remaining_incorrect_country:
+            remaining_incorrect_country.append(random_incorrect_country)
+
+    for country in remaining_incorrect_country:
+        remaining_countries.append(country)
+
     return remaining_countries
-
-
 
 
 if __name__ == "__main__":
