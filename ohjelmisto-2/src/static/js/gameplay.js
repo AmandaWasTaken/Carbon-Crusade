ba145468b4c95034ff2274a35695170026ec12f5
Juho Molin <juho.molin@metropolia.fi>
2024-12-12 01:47:29 +0200

    gameplay js testauksia

--- ohjelmisto-2/src/static/js/gameplay.js
+++ ohjelmisto-2/src/static/js/gameplay.js
@@ -37,7 +37,6 @@ function close_info(){
 
 async function button_click(answer_number){
   if (answer_number >= 1 && answer_number <= 6){
-    // console.log(gameData)
     const player_answer = answer_number
     const countries = gameData.all_country_options
     const correct_answer = gameData.current_country[1];
@@ -53,25 +52,51 @@ async function button_click(answer_number){
     });
     const data = await response.json();
 
-    // console.log(data)
-    // console.log("done")
-    // console.log(data.success)
     if (data.success === false){
       document.getElementById('button' + answer_number).classList = "button-disabled"
       document.getElementById('button' + answer_number).setAttribute("onclick", "")
+      const remaining_list = []
+      for (let i = 1; i < 7; i++){
+        if (document.getElementById('button'+i).className === "button"){
+          remaining_list.push(i)
+        } else {
+          remaining_list.push(0)
+        }
+      }
+      console.log(remaining_list, countries.indexOf(correct_answer))
+      const response = await fetch('/remove_random_answers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          remaining_country_indexes: remaining_list,
+          correct_country_index: countries.indexOf(correct_answer) })
+        });
+      const data2 = await response.json();
+      // console.log(data2)
+      document.getElementById('button' + data2.selection).classList = "button-disabled"
+      document.getElementById('button' + data2.selection).setAttribute("onclick", "")
+      if (document.getElementById('heart1').className === "heart"){
+        document.getElementById('heart1').classList = "no-heart"
+        document.getElementById('heart1').setAttribute("src", "../static/Images/heart_empty.svg")
+      } else if (document.getElementById('heart2').className === "heart"){
+        document.getElementById('heart2').classList = "no-heart"
+        document.getElementById('heart2').setAttribute("src", "../static/Images/heart_empty.svg")
+      } else if (document.getElementById('heart2').className === "no-heart"){
+        // document.getElementById('heart3').classList = "no-heart"
+        // document.getElementById('heart3').setAttribute("src", "../static/Images/heart_empty.svg")
+        document.getElementById('random-event').innerHTML = "Unfortunately you answered wrong and didn't gain any points"
+        open_event()
+      }
     }
 
     if (data.success === true){
       document.getElementById('random-event').innerHTML = "Correct!<br>Now choose where you want to fly."
       document.getElementById('button1').setAttribute("onclick", "button_click(7)")
-      // console.log("asd")
       open_event()
     }
   } else if (answer_number >= 7 && answer_number <= 12){
-    // console.log("APUA")
-    // console.log(answer_number-6, gameData.wrong_countries[0], document.getElementById('turns').innerHTML, gameData.current_country[1])
-    // console.log(gameData.all_country_options)
-    // console.log(gameData)
     const response = await fetch('/count_player_points', {
         method: 'POST',
         headers: {
@@ -84,15 +109,12 @@ async function button_click(answer_number){
           turns_left: document.getElementById('turns').innerHTML })
     });
     const data = await response.json();
-    // console.log("toimi")
-    // console.log(data)
     if (data.did_event_happen === true){
       const content = "You decided to fly to " + gameData.all_country_options[answer_number-7] + "!"
       document.getElementById('random-event').innerHTML = content
       open_event()
       gameData = await get_new_question();
       for (let i = 1; i < 7; i++){
-        // console.log("asd")
         document.getElementById('button' + i).classList = "button"
         document.getElementById('button' + i).setAttribute("onclick", "button_click("+i+")")
       }
@@ -103,14 +125,11 @@ async function button_click(answer_number){
       open_event()
       gameData = await get_new_question();
       for (let i = 1; i < 7; i++){
-        // console.log("asd")
         document.getElementById('button' + i).classList = "button"
         document.getElementById('button' + i).setAttribute("onclick", "button_click("+i+")")
       }
     }
-    //document.getElementById('random-event').innerHTML = ""
   }
-
 }
 
 
