c1a97be6940fc03921805fdd3e2e1f6cf33ae9f8
jerep <jerepun@metropolia.fi>
2024-12-11 15:54:29 +0200

    50/50 function finished

--- ohjelmisto-2/src/lifeline.py
+++ ohjelmisto-2/src/lifeline.py
@@ -14,7 +14,7 @@ def fiftyfifty(country_options, correct_answer):
         if country == correct_answer:
             remaining_countries.append(country)
 
-    while len(remaining_countries) != int(len(countries) / 2):
+    while len(remaining_incorrect_country) + 1 != int(len(countries) / 2):
         random_incorrect_country = random.choice(incorrect_countries)
         if random_incorrect_country not in remaining_incorrect_country:
             remaining_incorrect_country.append(random_incorrect_country)
@@ -26,7 +26,7 @@ def fiftyfifty(country_options, correct_answer):
 
 
 if __name__ == "__main__":
-    countries = ["suomi","ruotsi","viro","saksa"]
+    countries = ["suomi","ruotsi","viro","saksa","norja","tanska"]
     correct_country = random.choice(countries)
 
     print(countries)
